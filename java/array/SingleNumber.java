529a9e226027c5a6281251681662066055299d0d
ShubhKotnala <skshubhankar@gmail.com>
2022-10-12 22:43:06 +0530

    Added Single Number boilerplate code

---
+++ java/array/SingleNumber.java
@@ -0,0 +1,12 @@
+class SingleNumber {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,2,3,4,3,5,4,5};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        //TODO: implementation needed
+        return 0;
+    }
+}
---
+++ python/array/SingleNumber.py
@@ -0,0 +1,19 @@
+'''
+ Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ You must implement a solution with a linear runtime complexity and use only constant extra space.
+ 
+ Example 1:
+    Input: nums = [2,2,1]
+    Output: 1
+
+Example 2:
+    Input: nums = [4,1,2,1,2]
+    Output: 4
+ '''
+
+def single_number(nums):
+    result = 0
+    print(result)
+
+if __name__ == "__main__":
+    single_number([1,2,3,2,4,3,5,4,5])
