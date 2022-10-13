a65dba1bfc9b8debd93bf4f91862d383154fd5d0
Debayan97531 <debayankumar09@gmail.com>
2022-10-13 09:29:13 +0530

    Added

--- python/array/MountainArray.py
+++ python/array/MountainArray.py
@@ -33,6 +33,11 @@ Constraints:
 '''
 
 def valid_mountain_array(arr):
+    i=0;n=len(arr);j=n-1;
+    while i+1<n and arr[i]<arr[i+1] : i+=1
+    while j>0 and arr[j-1]>arr[j] : j-=1
+    if i>0 and i==j and j<n-1 :
+        return True
     return False
 
 
