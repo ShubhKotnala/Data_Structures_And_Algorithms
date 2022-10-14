45a70de163b195c22022f28c0de3a6d297db7c8f
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-10-14 22:56:37 +0530

    Code formatted...

--- java/array/FizzBuzz.java
+++ java/array/FizzBuzz.java
@@ -37,27 +37,24 @@ public class FizzBuzz {
         List<String> result = new ArrayList<>();
         for(int i=1;i<=n;i++)
         {
-
-        if(i%3==0 && i%5==0)
-        {
-            
-            result.add("FizzBuzz");
-            continue;
-        }
-        else if(i % 3 == 0)
-        {
-            
-            result.add("Fizz");
-            continue;
-        }
-        else if(i % 5 == 0)
-        {
-            result.add("Buzz");
-            continue;
-        }
-        else{
-        result.add(Integer.toString(i));
-        }     
+            if(i%3==0 && i%5==0)
+            {
+                result.add("FizzBuzz");
+                continue;
+            }
+            else if(i % 3 == 0)
+            {
+                result.add("Fizz");
+                continue;
+            }
+            else if(i % 5 == 0)
+            {
+                result.add("Buzz");
+                continue;
+            }
+            else{
+                result.add(Integer.toString(i));
+            }     
         }
         return result;
     }
