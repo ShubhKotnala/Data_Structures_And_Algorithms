b53335596eef07f3b468ccf5f102aaa8bddb262a
jxy0927 <91708783+jxy0927@users.noreply.github.com>
2023-10-31 21:47:31 +0800

    Update quick_sort.cpp

--- CPP/Sorting/quick_sort.cpp
+++ CPP/Sorting/quick_sort.cpp
@@ -1,3 +1,15 @@
+/*
+Quick Sort is a common sorting algorithm that sorts an array or list using a strategy of Divide and Conquer.
+he basic idea behind quicksort is as follows:
+Select an element from the array as the pivot.
+Moves elements in the array smaller than the base to the left of the base and elements larger than the base to the right of the base. This step is called a partition operation.
+Recursively sorts the subarray to the left of the base and the subarray to the right.
+This process is repeated until the entire array is in order. The key of fast sorting is to select the appropriate base element and clever partitioning algorithm to ensure the efficiency of sorting
+
+input arr[] = {10, 7, 8, 9, 1, 5}
+output arr[] = {1, 5, 7, 8, 9, 10}
+*/
+
 #include<iostream>
 using namespace std;
 
